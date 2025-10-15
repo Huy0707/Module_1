122f008f48ada5c73fe31cfc9dbfe16263c5defe
Huy0707 <tranhoanggiahuy08@gmail.com>
2025-10-15 21:27:00 +0700

    up bai tap

---
+++ ss6/[Thực hành]Hiển thị đầu ra.js
@@ -0,0 +1,15 @@
+// function showmessage(){
+//     alert('hello');
+// }
+
+// function showmessage() {
+//     document.getElementById('chat').innerHTML = "Hello world!" ;
+// }
+
+// function showmessage() {
+//     document.write('hello') ;
+// }
+
+// function showmessage() {
+//     console.log('Hello');
+// }
