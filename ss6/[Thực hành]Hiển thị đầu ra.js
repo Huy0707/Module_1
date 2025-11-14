a83d6a2e5ad934895eb6f2d9e00c392d57697068
Trần Hoàng Gia Huy <tranhoanggiahuy08@MacBook-Air-cua-Tran.local>
2025-11-14 20:27:15 +0700

    up bai tap

--- ss6/[Thực hành]Hiển thị đầu ra.js
+++ ss6/[Thực hành]Hiển thị đầu ra.js
@@ -1,15 +1,15 @@
-// function showmessage(){
-//     alert('hello');
-// }
+function showmessage(){
+    alert('hello');
+}
 
-// function showmessage() {
-//     document.getElementById('chat').innerHTML = "Hello world!" ;
-// }
+function showmessage() {
+    document.getElementById('chat').innerHTML = "Hello world!" ;
+}
 
-// function showmessage() {
-//     document.write('hello') ;
-// }
+function showmessage() {
+    document.write('hello') ;
+}
 
-// function showmessage() {
-//     console.log('Hello');
-// }
+function showmessage() {
+    console.log('Hello');
+}
